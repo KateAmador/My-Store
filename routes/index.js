cc0309442cbee0c37b5c4c2a2115e138edb0d892
Kamador <kateamador13@gmail.com>
2022-08-21 02:41:42 -0500

    [deploy]: deploy en heroku

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 
 const productsRouter = require('./productsRouter');
-const usersRouter = require('./usersRouter');
 const categoriesRouter = require('./categoriesRouter');
+const usersRouter = require('./usersRouter');
+const orderRouter = require('./ordersRouter');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
-  router.use('/users', usersRouter);
   router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
+  router.use('/orders', orderRouter);
 }
 
 module.exports = routerApi;

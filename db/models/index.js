47fcec28af60e36cc06ff1e5fcd770587b7c4dc3
Kamador <kateamador13@gmail.com>
2022-09-03 01:59:20 -0500

    Relacion uno a uno

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,12 @@
 const { User, UserSchema } = require('./userModel');
+const { Customer, CustomerSchema } = require('./customerModel');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+
+  User.associate(sequelize.models);
+  Customer.associate(sequelize.models);
 }
 
 module.exports = setupModels;

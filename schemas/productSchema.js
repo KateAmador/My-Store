74179e5c00858ee9054fe55faff5b6c39078ecfd
Kamador <kateamador13@gmail.com>
2022-09-03 15:55:16 -0500

    Paginacion

--- schemas/productSchema.js
+++ schemas/productSchema.js
@@ -1,11 +1,14 @@
-const joi = require("joi");
+const joi = require('joi');
 
-const id = joi.string().uuid();
+const id = joi.number().integer();
 const name = joi.string().min(3).max(15);
 const price = joi.number().integer().min(10);
 const description = joi.string().min(10);
 const image = joi.string().uri();
 const categoryId = joi.number().integer();
+
+const limit = joi.number().integer();
+const offset = joi.number().integer();
 
 const createProductSchema = joi.object({
   name: name.required(),
@@ -19,7 +22,7 @@ const updateProductSchema = joi.object({
   name: name,
   price: price,
   image: image,
-  description,
+  description: description,
   categoryId
 });
 
@@ -27,4 +30,9 @@ const getProductSchema = joi.object({
   id: id.required(),
 });
 
-module.exports = {createProductSchema, updateProductSchema, getProductSchema}
+const queryProductSchema = joi.object({
+  limit,
+  offset
+});
+
+module.exports = { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema }
